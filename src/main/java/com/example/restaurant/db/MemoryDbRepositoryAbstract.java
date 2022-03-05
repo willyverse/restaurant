e3abd488aa043c6b6ce1abafb2400b7959f2d96b
Chanyoung Kim <whqnd1106@gmail.com>
2022-03-06 00:36:50 +0900

    first commit

---
+++ src/main/java/com/example/restaurant/db/MemoryDbRepositoryAbstract.java
@@ -0,0 +1,51 @@
+package com.example.restaurant.db;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+// MemoryDbRepository에 들어가는 모든 Type의 데이터는 MemoryDbEntity를 상속받기 때문에 getIndex할 수 있다.
+abstract public class MemoryDbRepositoryAbstract<T extends MemoryDbEntity> implements MemoryDbRepositoryIfs<T>{
+
+    private final List<T> db = new ArrayList<>();
+    private int index = 0;
+
+    @Override
+    public Optional<T> findById(int index) {
+        return db.stream().filter(it -> it.getIndex() == index).findFirst();
+    }
+
+    @Override
+    public T save(T entity) {
+
+        var optionalEntity = db.stream().filter(it -> it.getIndex() == entity.getIndex()).findFirst();
+
+        if(optionalEntity.isEmpty()) {
+            // db에 데이터가 없는 경우
+            index++;
+            entity.setIndex(index);
+            db.add(entity);
+            return entity;
+        } else {
+            // db에 이미 데이터가 있는 경우
+            var preIndex = optionalEntity.get().getIndex();
+            entity.setIndex(preIndex);
+            deleteById(preIndex);
+            db.add(entity);
+            return entity;
+        }
+    }
+
+    @Override
+    public void deleteById(int index) {
+        var optionalEntity = db.stream().filter(it -> it.getIndex() == index).findFirst();
+        if(optionalEntity.isPresent()) {
+            db.remove(optionalEntity.get());
+        }
+    }
+
+    @Override
+    public List<T> listAll() {
+        return db;
+    }
+}
